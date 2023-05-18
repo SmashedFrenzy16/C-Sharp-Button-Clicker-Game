393b478679d8f6bab56bfe3a9b44164c89c4dc1a
SmashedFrenzy16 <xbop.grillion@gmail.com>
2023-05-18 11:06:13 +0100

    Add files via upload

---
+++ ButtonClickerGame/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonClickerGame
+{
+    public partial class Form1 : Form
+    {
+        private int clicks = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClick_Click(object sender, EventArgs e)
+        {
+
+            valueLabel.Text = clicks.ToString();
+
+            clicks++;
+        }
+    }
+}
