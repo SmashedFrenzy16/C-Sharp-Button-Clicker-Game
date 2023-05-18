5f47865147ed11b2e470862377119fe0ff0f4e8c
SmashedFrenzy16 <xbop.grillion@gmail.com>
2023-05-18 11:06:31 +0100

    Add files via upload

---
+++ ButtonClickerGame/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ButtonClickerGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonClick = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.valueLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonClick
+            // 
+            this.buttonClick.Location = new System.Drawing.Point(308, 138);
+            this.buttonClick.Name = "buttonClick";
+            this.buttonClick.Size = new System.Drawing.Size(188, 149);
+            this.buttonClick.TabIndex = 0;
+            this.buttonClick.Text = "Click This Button!";
+            this.buttonClick.UseVisualStyleBackColor = true;
+            this.buttonClick.Click += new System.EventHandler(this.buttonClick_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(192, 58);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(447, 25);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "Button Clicker Game By @SmashedFrenzy16";
+            // 
+            // valueLabel
+            // 
+            this.valueLabel.AutoSize = true;
+            this.valueLabel.Location = new System.Drawing.Point(377, 335);
+            this.valueLabel.Name = "valueLabel";
+            this.valueLabel.Size = new System.Drawing.Size(24, 25);
+            this.valueLabel.TabIndex = 2;
+            this.valueLabel.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.valueLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.buttonClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClick;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label valueLabel;
+    }
+}
+
